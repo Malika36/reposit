3fd3508c75a870ff989f9c16aafac70d74b88a00
Malika36 <ianassss@mail.ru>
2020-06-20 21:26:38 +0300

    NextCommit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
         System.out.println("SecondCommit");
-        System.out.println("NextCommit");
+        System.out.println("NextCommi");
     }
 }
